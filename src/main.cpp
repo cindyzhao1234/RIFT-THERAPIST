8ef1ffd9dbedabc815b52d72ca6c7ee32e804e3f
Cindy Zhao <zhaoxin.dy@gmail.com>
2026-05-20 00:33:28 +1000

    FEAT: Added API request and JSON dependencies

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <cstdlib>
+
+#include <cpr/cpr.h>
+#include <nlohmann/json.hpp>
 
 int main() {
     std::cout << "Rift Therapist is running!" << std::endl;
+    
+    const char* apiKey = std::getenv("RIOT_API_KEY");
+
+    if(apiKey == nullptr){
+        std::cout << "API key missing" << std::endl;
+    } else{
+        std::cout << "API key found" << std::endl;
+    }
     return 0;
 }
